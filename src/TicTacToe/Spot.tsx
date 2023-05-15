7ed4a806695a94e08870ac4a1c7fbda47106aa06
zdubin <52575128+zdubin@users.noreply.github.com>
2023-05-15 16:21:48 -0400

    feat(Tie Tac Toe): Remove select buttons, if game is won

--- src/TicTacToe/Spot.tsx
+++ src/TicTacToe/Spot.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import { XorO } from '../AppTypes/AppTypes'
 import { Button } from "reactstrap";
 
-export const Spot: React.FC<{spotValue: XorO, didWin: () => XorO | null, setPiece: () => void}> = ({spotValue, didWin, setPiece}) => {
-    
+export const Spot: React.FC<{ spotValue: XorO, didWin: () => XorO | null, setPiece: () => void }> = ({ spotValue, didWin, setPiece }) => {
+
     return <>
-    {   spotValue ??
-        <Button className='button-space' variant="contained" onClick={() => setPiece()} disabled={didWin() ? true : false}>Select</Button>
-    }
-    </>   
+        {spotValue ?? (!didWin() ?
+            <Button className='button-space' variant="contained" onClick={() => setPiece()} >Select</Button> : null
+        )
+        }
+    </>
 }
