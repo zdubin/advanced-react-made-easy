559f5e45040e65ea63ea69f1db189412c48d2901
zdubin <52575128+zdubin@users.noreply.github.com>
2023-05-16 12:32:32 -0400

    feat(Tie Tac Toe): Refactored and added highlighting of cells of winning tiles

--- src/TicTacToe/TicTacToe.tsx
+++ src/TicTacToe/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState, useEffect } from "react";
 import { Button } from "reactstrap";
 
 import { useXorO } from "../CustomHooks/useXorO";
@@ -15,9 +15,10 @@ const TicTacToe: React.FC = () => {
         [null, null, null]
     ];
 
-    const [currentXorO, setNextMove]:[() => XorO, () => void] = useXorO();
+    const [currentXorO, setNextMove]: [() => XorO, () => void] = useXorO();
     const [board, setBoard] = useState<BoardSquares>(
         [...emptyBoard]);
+    const [winningSquares, setWinningSquares] = useState<number[][] | null>(null);
 
     const setPiece = (col: number, row: number) => {
         const nextBoard = [...board];
@@ -26,35 +27,63 @@ const TicTacToe: React.FC = () => {
         setBoard(nextBoard);
     }
 
-    const didWin = useCallback((): XorO | null => {
-        // diag win
 
-        console.table(board)
-        if ((board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
-            (board[0][2] === board[1][1] && board[1][1] === board[2][0]) ) // && (board[1][1] === 'X' || board[1][1] === 'O'))
-            {
- //               console.log(board[0][0], board[1][1] , board[2][2])
- //               console.log(board[0][2], board[1][1] , board[2][0])
-                return board[1][1];
-            }
+    const getWinningCombo = useCallback((): number[][] | null => {
+
+        if (board[2][2] && (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]))
+            return [[0, 0], [1, 1], [2, 2]];
+        else if (board[2][0] && board[0][2] === board[1][1] && board[1][1] === board[2][0])
+            return [[0, 2], [1, 1], [2, 0]];
 
         for (let i = 0; i < 3; i++) {
-            if (board[i][0] === board[i][1] && board[i][1] === board[i][2])
-            {
-//                console.log(board[i][0] ,board[i][1] ,board[i][2])
-                return board[i][0];
+            if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                //                console.log(board[i][0] ,board[i][1] ,board[i][2])
+                return [[i, 0], [i, 1], [i, 2]];
             }
         }
 
         for (let j = 0; j < 3; j++)
-            if (board[0][j] === board[1][j] && board[1][j] === board[2][j])
-            {
- //               console.log(board[0][j] , board[1][j] , board[2][j])
-                return board[0][j];
+            if (board[0][j] && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
+                //               console.log(board[0][j] , board[1][j] , board[2][j])
+                return [[0, j], [1, j], [2, j]];
             }
 
         return null;
-    },[board])
+    }, [board])
+
+    useEffect(() => {
+        setWinningSquares(getWinningCombo);
+    }, [getWinningCombo]);
+
+    const didWin = useCallback((): XorO | null => {
+        // diag win
+        /*
+                console.table(board)
+                if ((board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+                    (board[0][2] === board[1][1] && board[1][1] === board[2][0]) ) // && (board[1][1] === 'X' || board[1][1] === 'O'))
+                    {
+         //               console.log(board[0][0], board[1][1] , board[2][2])
+         //               console.log(board[0][2], board[1][1] , board[2][0])
+                        return board[1][1];
+                    }
+        
+                for (let i = 0; i < 3; i++) {
+                    if (board[i][0] === board[i][1] && board[i][1] === board[i][2])
+                    {
+        //                console.log(board[i][0] ,board[i][1] ,board[i][2])
+                        return board[i][0];
+                    }
+                }
+        
+                for (let j = 0; j < 3; j++)
+                    if (board[0][j] === board[1][j] && board[1][j] === board[2][j])
+                    {
+         //               console.log(board[0][j] , board[1][j] , board[2][j])
+                        return board[0][j];
+                    }
+        */
+        return winningSquares ? board[winningSquares[0][0]][winningSquares[0][1]] : null;
+    }, [board, winningSquares])
 
     const memoizedDidWin = useMemo<XorO | null>(() => didWin(), [didWin])
 
@@ -67,7 +96,8 @@ const TicTacToe: React.FC = () => {
             <div className="game-board">
                 {
                     board.map((row, i) =>
-                        row.map((_, j) => <div key={`row_${i}_${j}`} className="box">
+                        row.map((_, j) => <div key={`row_${i}_${j}`}
+                            className={winningSquares?.some(([a, b]) => a === i && b === j) ? "box box__winning" : "box"}>
                             <Spot spotValue={board[i][j]} setPiece={() => setPiece(i, j)} didWin={() => memoizedDidWin} />
                         </div>
                         )
@@ -76,7 +106,7 @@ const TicTacToe: React.FC = () => {
 
             </div>
             <Button className='button-space' color="warning" onClick={() => setBoard([...emptyBoard])
-                                                        }>Reset</Button>
+            }>Reset</Button>
         </>
     )
 }
